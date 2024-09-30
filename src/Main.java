7f92355d16345ff132988114fdc01c8445be6e0c
Josef Hedblom <hedblomjosef@gmail.com>
2024-09-30 21:58:23 +0200

    Update Main to take user input to find a plant and give the plant water.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
-public class Main {
-    public static void main(String[] args) {
-        Cactus cactus = new Cactus("Kaktus","Igge", 20);
-        Palm plam1 = new Palm("Plam","Laura", 5);
-        Palm plam2 = new Palm("Plam","Olof", 5);
-        CarnivorousPlant carnivorousPlant = new CarnivorousPlant("Köttätande växt","Meatloaf", 0.7);
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
-        System.out.println(cactus);
-        System.out.println(plam1);
-        System.out.println(plam2);
-        System.out.println(carnivorousPlant);
+public class Main {
+
+
+    public static void main(String[] args) {
+        String vilkenVäxtInput = JOptionPane.showInputDialog("Vilken växt ska få mat?");
+
+        List<Plant> plants = new ArrayList<>();
+
+        Cactus igge = new Cactus("Kaktus", "Igge", 0.2);
+        Palm laura = new Palm("Plam", "Laura", 3);
+        Palm olof = new Palm("Plam", "Olof", 0.5);
+        CarnivorousPlant meatloaf = new CarnivorousPlant("Köttätande växt", "Meatloaf", 0.5);
+
+        plants.add(meatloaf);
+        plants.add(igge);
+        plants.add(laura);
+        plants.add(olof);
+
+        for (Plant plant : plants) {
+            if(plant.getName().equalsIgnoreCase(vilkenVäxtInput)){
+                printMessage(plant);
+            }
+        }
+
+    }
+
+    public static void printMessage(Plant plant){
+        JOptionPane.showMessageDialog(null, plant.getFluidType());
     }
 }
